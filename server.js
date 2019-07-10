51a2c414b7ba8468ac481ccbca9d3092d85838a2
lara bergerson <lara.bergerson@gmail.com>
2019-07-10 15:47:25 -0500

    set up file paths

---
+++ server.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+// Series of npm packages to give the server functionality
+var express = require("express");
+
+// Communicate to node that we need an "express" server
+var app = express();
+
+// Set initial port.
+var PORT = process.env.PORT || 8080;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER - points server to "route" files -> maps out how server should respond
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER -> starts/initiated the server
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
